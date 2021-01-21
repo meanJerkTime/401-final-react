a61610e5ddb74b89391eaed95ea7512c9ee6fadc
EDGAR ROMERO <EDGAROMERO1987@GMAIL.COM>
2021-01-20 17:04:13 -0800

    wired up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import GameBoard from './components/game-board/gameBoard.js';
+import './components/game-board/gameBoard.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GameBoard />
+    </>
   );
 }
 
---
+++ src/components/game-board/gameBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './gameBoard.scss';
+
+
+
+function GameBoard() {
+  return (
+    <>
+      <h1> GAME HEHEHE BOARD </h1>
+    </>
+  );
+}
+
+export default GameBoard;
+

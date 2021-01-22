278755b8d711784bdb419a3c92be1b06c78632f5
Chris-Bortel <bortelcc19@gmail.com>
2021-01-21 21:16:26 -0800

    Working on the about page. The transition on the card is not currently working. Additionally, I think that I am going to change the animation to be an on hover get larger affect instead of a flip

--- src/components/aboutPage/aboutPage.js
+++ src/components/aboutPage/aboutPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Navbar from '../header/navbar/navbar.js';
 import Footer from '../footer/footer.js';
-
+import './aboutPage.scss';
 export default function aboutPage() {
   return (
     <div>
       <Navbar/>
-      <h1>ABOUT PAGE</h1>
+      <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h2>Jane Doe<br /><span>Senior Designer</span></h2>
+            <div className="social-icons">
+                <a href="#" className="fa fa-facebook" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-google-plus" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+                <a href="#" className="fa fa-instagram" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
       <Footer/>
     </div>
   )
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ import './footer.scss';
 export default function Footer() {
   return (
     <>
-      <footer><a href="https://github.com/meanJerkTime" target="_blank">&copy; 2021 Mean Jerk Time</a></footer>
+      <footer><a href="https://github.com/meanJerkTime" target="_blank" rel="noreferrer">&copy; 2021 Mean Jerk Time</a></footer>
     </>
   );
 }

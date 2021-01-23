e468e66c30ab4eb20ac0a222f3b6a9d0c52caef0
Chris-Bortel <bortelcc19@gmail.com>
2021-01-22 16:18:03 -0800

    Working on the about page, I am struggling with flex box, I might use grid instead

--- src/components/aboutPage/aboutPage.js
+++ src/components/aboutPage/aboutPage.js
@@ -6,22 +6,103 @@ export default function aboutPage() {
   return (
     <div>
       <Navbar/>
+     
+<section>
+
       <div className="card">
         <div className="card-front"></div>
         <div className="card-back">
-            <h2>Jane Doe<br /><span>Senior Designer</span></h2>
+            <h4>Diane Stephani<br /><span>Game Logic</span></h4>
             <div className="social-icons">
-                <a href="#" className="fa fa-facebook" aria-hidden="true"></a>
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
                 <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
-                <a href="#" className="fa fa-google-plus" aria-hidden="true"></a>
                 <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
-                <a href="#" className="fa fa-instagram" aria-hidden="true"></a>
             </div>
         </div>
     </div>
+
+{/* Backend */}
+<div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h4>Peng Chen<br /><span>Backend</span></h4>
+            <div className="social-icons">
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+
+{/* Game Logic */}
+    <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h4>Joe Pennock<br /><span>Game Logic</span></h4>
+            <div className="social-icons">
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h4>Kory Jackson<br /><span>Game Logic</span></h4>
+            <div className="social-icons">
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+
+
+{/* UI/UX */}
+    <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h4>Brendon Hampton<br /><span>UI/UX</span></h4>
+            <div className="social-icons">
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h4>Edgar Romero<br /><span>UI/UX</span></h4>
+            <div className="social-icons">
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+
+    <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back">
+            <h4>Chris Bortel<br /><span>UI/UX</span></h4>
+            <div className="social-icons">
+                <a href="#" className="fab fa-github" aria-hidden="true"></a>
+                <a href="#" className="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" className="fa fa-linkedin" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+
+
+
+</section>
+
       <Footer/>
     </div>
   )
 }
 
- 

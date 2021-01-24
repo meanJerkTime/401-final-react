4bbf5a78a29350378c950ed1adad6511b0aaa2d2
Chris-Bortel <bortelcc19@gmail.com>
2021-01-23 17:16:11 -0800

    We are mapping over the monster

--- src/components/game-table/combat-area/monster/monster.js
+++ src/components/game-table/combat-area/monster/monster.js
@@ -6,20 +6,23 @@ import './monster.scss';
 export default function Monster() {
   
   const monster = useSelector( state => state.monster)
+  const monster1 = monster[0]
+  console.log(monster)
   return (
     <>
       <div id="">
       <ul id='' >
-            {
-              monster.monster.map(item  => <li><Card key={Math.random()} className="zoom" style={{ width: '100px' }}>
-                    <Card.Img variant="top" src={item.image} />
-                    <li>{item.id}</li>
-                    <li>{item.name}</li>
-                </Card></li>
-            )
-            }
-          </ul> 
+      {
+        monster.monster.map(item  => <li><Card key={Math.random()} className="zoom" style={{ width: '100px' }}>
+              <Card.Img variant="top" src={item.image} />
+              <li>{item.id}</li>
+              <li>{item.name}</li>
+          </Card></li>
+      )
+      }
+      </ul> 
       </div>
+
     </>
   );
 }

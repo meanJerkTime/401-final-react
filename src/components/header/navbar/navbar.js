15903bc241fcda674b2c7e6caf7d48e7baccbaad
BrendonLH <Brendonhampton1988@gmail.com>
2021-01-24 03:47:58 -0800

    fixed navbar to include a signup piece

--- src/components/header/navbar/navbar.js
+++ src/components/header/navbar/navbar.js
@@ -6,6 +6,9 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
 
+import './navbar.scss';
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -20,21 +23,32 @@ const useStyles = makeStyles((theme) => ({
 export default function Navbar() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-          <NavLink to="/">Home</NavLink>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <NavLink to='/rules'>Rules</NavLink>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <NavLink to="/about">About</NavLink>
-          </Typography>
-          <Button id='signUp' color="inherit">Signup</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <>
+      <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to='/rules'>Rules</NavLink>
+      
+        <div className="dropdown">
+          <button className="dropbtn">Signup
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            <form>
+              <label for='username'>Username</label>
+              <input type='text' id='username' name='username'/>
+              <label for='password'>Password</label>
+              <input type='password' id='password' name='password'/>
+            </form>
+            <button>Submit</button>
+          </div>
+        </div> 
+        <NavLink to="/about">About</NavLink>
+      </div>
+    </>
+
+
+
+
+   
   );
 }

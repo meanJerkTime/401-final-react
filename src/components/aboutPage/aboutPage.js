b5d684974707de47bf69e4f3be7c66b5495cb0c1
Chris-Bortel <bortelcc19@gmail.com>
2021-01-25 17:29:22 -0800

    about page

--- src/components/aboutPage/aboutPage.js
+++ src/components/aboutPage/aboutPage.js
@@ -9,21 +9,160 @@ export default function aboutPage() {
   
 
     <div class="grid-container">
-    <div class="diane about-card">
-    <p>Diane Stephani <br/> <span>Project Manager</span></p>
+    
+        <div class="diane about-card">
+            
+            <h4>Diane Stephani <p className="team">Project Manager</p></h4>
         
+        <article>
+            <p> 
+            Simplified personal pitch (where you're from, what your goals are, etc.)
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
         <div className="social-icons">
             <a href="https://github.com/dianestephani" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
             {/* <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
             <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a> */}
         </div>
     </div>
-    <div class="peng about-card">Peng Chen</div>
-    <div class="joe about-card">Joe Pennock</div>
-    <div class="kory about-card">Kory Jackson</div>
-    <div class="brendon about-card">Brendon Hampton</div>
-    <div class="chris about-card">Chris Bortel</div>
-    <div class="edgar about-card">Edgar Romero</div>
+  
+
+
+
+    <div class="peng about-card">
+            <h4>Peng Chen <p className="team">Backend</p></h4>
+        
+        <article>
+            <p> 
+            Simplified personal pitch (where you're from, what your goals are, etc.)
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
+        <div className="social-icons">
+            <a href="#" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+            {/* <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
+            <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a> */}
+        </div>
+    </div>
+
+    <div class="joe about-card">
+            <h4>Joe Pennock <p className="team">Game Logic</p></h4>
+        
+        <article>
+            <p> 
+            Simplified personal pitch (where you're from, what your goals are, etc.)
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
+        <div className="social-icons">
+            <a href="#" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+            {/* <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
+            <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a> */}
+        </div>
+    </div>
+
+
+
+    <div class="kory about-card">
+            <h4>Kory Jackson<p className="team">Game Logic</p></h4>
+        
+        <article>
+            <p> 
+            Simplified personal pitch (where you're from, what your goals are, etc.)
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
+        <div className="social-icons">
+            <a href="#" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+            {/* <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
+            <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a> */}
+        </div>
+    </div>
+
+   
+    <div class="brendon about-card">
+            <h4>Brendon Hampton<p className="team">UI / UX</p></h4>
+        
+        <article>
+            <p> 
+            Simplified personal pitch (where you're from, what your goals are, etc.)
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
+        <div className="social-icons">
+            <a href="#" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+            {/* <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
+            <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a> */}
+        </div>
+    </div>
+
+
+
+    <div class="chris about-card">
+            <h4>Chris Bortel <p className="team">UI / UX</p></h4>
+        
+        <article>
+            <p> 
+            {/* Simplified personal pitch (where you're from, what your goals are, etc.) */}
+            The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
+        <div className="social-icons">
+            <a href="https://github.com/Chris-Bortel" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+
+            <a href="https://twitter.com/bortel_chris" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+
+            <a href="https://www.linkedin.com/in/chris-bortel/" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+        </div>
+    </div>
+
+
+
+
+
+    <div class="edgar about-card">
+            <h4>Edgar Romero<p className="team">UI / UX</p></h4>
+        
+        <article>
+            <p> 
+            Simplified personal pitch (where you're from, what your goals are, etc.)
+            <br/>
+            <br/>
+            Fun fact:
+            </p>
+        </article>
+            {/* image */}
+        <div className="social-icons">
+            <a href="#" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
+            {/* <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
+            <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a> */}
+        </div>
+    </div>
+
+
+
+
+
     </div>
     <div class="footer"><Footer/></div>
     
@@ -40,17 +179,7 @@ export default function aboutPage() {
 
 
 
-<div className="card">
-         <div className="card-front"></div>
-         <div className="card-back">
-             <h4>Diane Stephani<br /><span>Game Logic</span></h4>
-             <div className="social-icons">
-                 <a href="https://github.com/dianestephani" className="fab fa-github" aria-hidden="true" target="_blank" rel="noreferrer"> </a>
-                 <a href="#" className="fa fa-twitter" aria-hidden="true" target="_blank" rel="noreferrer"></a>
-                 <a href="#" className="fa fa-linkedin" aria-hidden="true" target="_blank" rel="noreferrer"></a>
-             </div>
-         </div>
-     </div>
+
 
 // {/* Backend */}
 // <div className="card">

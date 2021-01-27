160eb2482c630b436f0c983e8ac8295989ddefe3
Chris-Bortel <bortelcc19@gmail.com>
2021-01-27 14:53:30 -0800

    Displaying one monster card from the monster api

--- src/components/game-table/monster/monster.js
+++ src/components/game-table/monster/monster.js
@@ -1,12 +1,17 @@
-import React from 'react';
-// import {useSelector} from 'react-redux';
-// import Card from 'react-bootstrap/Card';
+import {React, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import Card from 'react-bootstrap/Card';
+import * as actions from '../../../store/monsterReducer.js'
 
 import './monster.scss';
 
 export default function Monster() {
-  
-  // const monster = useSelector( state => state.monster)
+  const dispatch = useDispatch()
+  const monster = useSelector( state => state.monster)
+
+  useEffect(()=> {
+    dispatch(actions.getMonster())
+  }, [])
   // const monster1 = monster[0]
   // console.log(monster)
   return (
@@ -14,14 +19,14 @@ export default function Monster() {
       <div className="monster-container">
       hello
       <ul>
-      {/* {
-      monster.monster.map(item  => <li><Card key={Math.random()} className="zoom" style={{ width: '100px' }}>
+      {
+      monster.monster.slice(0,1).map(item  => <li><Card key={Math.random()} className="zoom" style={{ width: '100px' }}>
             <Card.Img variant="top" src={item.image} />
             <li>{item.id}</li>
             <li>{item.name}</li>
         </Card></li>
       )
-      } */}
+      }
       </ul> 
       </div>
 

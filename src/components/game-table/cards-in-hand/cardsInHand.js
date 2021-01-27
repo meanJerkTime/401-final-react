45d56c567a67db62a1c0f21643c9b4ecf245da64
Chris-Bortel <bortelcc19@gmail.com>
2021-01-27 14:19:48 -0800

    We are recieving item cards from the api

--- src/components/game-table/cards-in-hand/cardsInHand.js
+++ src/components/game-table/cards-in-hand/cardsInHand.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import Card from 'react-bootstrap/Card';
 import * as actions from '../../../store/gameTableReducer.js'
@@ -17,6 +17,10 @@ export default function CardsInHand() {
     dispatch(actions.activateCard(card))
     dispatch(actions.removeCardFromHand(card))
   }
+
+  useEffect(()=> {
+    dispatch(actions.getCardsInHand())
+  }, [])
 
   return (
     <>
--- src/components/game-table/monster/monster.js
+++ src/components/game-table/monster/monster.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import Card from 'react-bootstrap/Card';
+// import {useSelector} from 'react-redux';
+// import Card from 'react-bootstrap/Card';
 
 import './monster.scss';
 
 export default function Monster() {
   
-  const monster = useSelector( state => state.monster)
+  // const monster = useSelector( state => state.monster)
   // const monster1 = monster[0]
-  console.log(monster)
+  // console.log(monster)
   return (
     <>
       <div className="monster-container">
       hello
       <ul>
-      {
+      {/* {
       monster.monster.map(item  => <li><Card key={Math.random()} className="zoom" style={{ width: '100px' }}>
             <Card.Img variant="top" src={item.image} />
             <li>{item.id}</li>
             <li>{item.name}</li>
         </Card></li>
       )
-      }
+      } */}
       </ul> 
       </div>
 

c4c030906795831662414e8a97795e6110a17523
Dirrbick <kory.a.jaxn@gmail.com>
2021-01-27 18:05:34 -0800

    pulling from staged

--- src/components/player-hub/create-a-room/createRoom.js
+++ src/components/player-hub/create-a-room/createRoom.js
@@ -8,9 +8,8 @@ export default function CreateRoom(props) {
 
   return (
     <>
-      {/* <button className='roomCreate' onClick={props.createRoom}>Create a room</button> */}
       <div>
-        <Button variant="outline-dark" size="lg">Create a room</Button>
+        <Button onClick={props.createRoom} variant="outline-dark" size="lg">Create a room</Button>
       </div>
     </>
   );
